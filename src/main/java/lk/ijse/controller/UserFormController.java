c450bced10b242439efbd8654bd10386de953d3d
Lakhini Voshadee Abeysundara <lakhinivoshadee24@gmail.com>
2024-11-08 23:13:45 +0530

    userlogin form fxml file

---
+++ src/main/java/lk/ijse/controller/UserFormController.java
@@ -0,0 +1,38 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class UserFormController {
+
+    @FXML
+    private Hyperlink hyperFP;
+
+    @FXML
+    private Hyperlink hyperSignUp;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void linkForgotPassword(ActionEvent event) {
+
+    }
+
+    @FXML
+    void linkSignUp(ActionEvent event) {
+
+    }
+
+}
