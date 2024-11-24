8c06d743f099b46dc459fd869e8a7605b86cbb16
Lakhini Voshadee Abeysundara <lakhinivoshadee24@gmail.com>
2024-11-24 23:38:15 +0530

    user entity & DTO

--- src/main/java/lk/ijse/dto/UserDTO.java
+++ src/main/java/lk/ijse/dto/UserDTO.java
@@ -1,4 +1,36 @@
 package lk.ijse.dto;
 
+import lk.ijse.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
+    private String id;
+    private String name;
+    private String password;
+    private String mobile;
+    private String email;
+    private String address;
+
+    public UserDTO(String id,String name,String password){
+        this.id=id;
+        this.name=name;
+        this.password=password;
+    }
+
+
+    public User toEntity(){
+        User user=new User();
+        user.setId(this.id);
+        user.setName(this.name);
+        user.setPassword(this.password);
+        user.setMobile(this.mobile);
+        user.setEmail(this.email);
+        user.setAddress(this.address);
+        return user;
+    }
 }
--- src/main/java/lk/ijse/entity/User.java
+++ src/main/java/lk/ijse/entity/User.java
@@ -1,4 +1,38 @@
 package lk.ijse.entity;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Cacheable
+@Table(name = "User")
 public class User {
+
+    @Id
+    @Column(name = "user_id")
+    private String id;
+
+
+    @Column(name = "user_name")
+    private String name;
+
+
+    @Column(name = "user_password")
+    private String password;
+
+    @Column(name = "user_mobile")
+    private String mobile;
+
+    @Column(name = "user_email")
+    private String email;
+
+    @Column(name = "user_address")
+    private String address;
 }
